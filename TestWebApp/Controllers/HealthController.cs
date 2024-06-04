4ea0e855504a21826cadc703863e1d7ae49f95d1
Grigory Zhukov <grigory@zhukoff.pro>
2024-06-04 21:52:02 +0300

    Добавил проект для тестов запуска докера

---
+++ TestWebApp/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            _logger.LogInformation("Health status requested");
+            return Ok(new { status = "OK" });
+        }
+    }
+}
